1d19b78171b5704a99545e66feabd0a70024c110
Steve Shreeve <steve.shreeve@gmail.com>
2019-02-07 23:23:53 -0700

    extend VueAsset, only override parse()

---
+++ VueCompactAsset.js
@@ -0,0 +1,29 @@
+const { Asset } = require('parcel-bundler');
+const VueAsset = require('parcel-bundler/lib/assets/VueAsset');
+const localRequire = require('parcel-bundler/src/utils/localRequire');
+
+const regex = /(?<=\n|^)(pug|coffee|stylus)(?:.*\n)([\s\S]*?)(\n(?=\S)|$)/g;
+const types = {
+  pug: 'template',
+  coffee: 'script',
+  stylus: 'style'
+};
+
+class VueCompactAsset extends VueAsset {
+  async parse(code) {
+    this.vueTemplateCompiler = await localRequire('vue-template-compiler', this.name);
+    this.vue = await localRequire('@vue/component-compiler-utils', this.name);
+
+    return this.vue.parse({
+      source: code.replace(regex, function (...match) {
+        return `<${types[match[1]]} lang='${match[1]}'>\n${match[2]}</${types[match[1]]}>\n`;
+      }),
+      needMap: this.options.sourceMaps,
+      filename: this.relativeName, // Used for sourcemaps
+      sourceRoot: '', // Used for sourcemaps. Override so it doesn't use cwd
+      compiler: this.vueTemplateCompiler
+    });
+  }
+}
+
+module.exports = VueCompactAsset;
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 module.exports = function (bundler) {
-  bundler.addAssetType('vue', require.resolve('./VueAsset.js'));
+  bundler.addAssetType('vue', require.resolve('./VueCompactAsset.js'));
 };
